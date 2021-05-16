60033a314da9d2d9e1a5bd1d4b9bbcf37c834131
Dayoung Kim <kimdayoung771@gmail.com>
2021-05-16 21:49:15 +0900

    make reset button

--- src/app.jsx
+++ src/app.jsx
@@ -48,6 +48,14 @@ class App extends Component {
         this.setState({ habits: habits })
     }
 
+    handleReset = () => {
+        const habits = this.state.habits.map(habit => {
+            habit.count = 0
+            return habit
+        })
+        this.setState({ habits })
+    }
+
   render() {
     return (
         <>
@@ -58,6 +66,7 @@ class App extends Component {
               onDecrement={this.handleDecrement}
               onDelete={this.handleDelete}
               onAdd={this.onAdd}
+              onReset={this.handleReset}
           />
         </>
 
