b75a868ed0a4112fdc7541616e4c6ad16b490942
클라라 (김다영) <clara@coinvest.kr>
2021-05-17 22:20:53 +0900

    class => function 구조로 변경

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,25 +1,26 @@
 import React, {PureComponent} from 'react';
 
-// PureComponent는 컴포넌트와 달리 이전의 프롭스와 스테이트를 비교해서 달라진 점이 없으면 re렌더링 하지 않음
+const HabitAddForm = (props) => {
+    const inputRef = React.createRef()
 
-class HabitAddForm extends PureComponent {
-    inputRef = React.createRef()
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault()
-        const name = this.inputRef.current.value
-        name && this.props.onAdd(name)
-        this.inputRef.current.value = ''
+        const name = inputRef.current.value
+        name && props.onAdd(name)
+        inputRef.current.value = ''
+    }
+    return (
+        <form className="add-form" onSubmit={onSubmit}>
+            <input ref={inputRef} type="text" className="add-input" placeholder="Habit" />
+            <button className="add-button">Add</button>
+        </form>
+    );
     }
 
-    render() {
-        return (
-            <form className="add-form" onSubmit={this.onSubmit}>
-                <input ref={this.inputRef} type="text" className="add-input" placeholder="Habit" />
-                <button className="add-button">Add</button>
-            </form>
-        );
-    }
-}
+;
 
 export default HabitAddForm;
+
+
+// PureComponent는 컴포넌트와 달리 이전의 프롭스와 스테이트를 비교해서 달라진 점이 없으면 re렌더링 하지 않음
+
